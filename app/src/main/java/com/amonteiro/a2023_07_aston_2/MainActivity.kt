0161e65dad748f551e90d94550fc6d506bb04ea9
Anthony <anth06ny@gmail.com>
2023-07-20 17:01:35 +0200

    Correction TP Manip écran P1

--- app/src/main/java/com/amonteiro/a2023_07_aston_2/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2023_07_aston_2/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.amonteiro.a2023_07_aston_2
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.amonteiro.a2023_07_aston_2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    //instancie les composants graphiques à la 1er utilisation grâce à lazy
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    //Callbck de la création de l'écran
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Affiche une interface graphique
+        setContentView(binding.root)
 
+        binding.btValidate.setOnClickListener {
+            binding.et.setText("Clic sur valider")
+        }
+
+        binding.btCancel.setOnClickListener(this)
 
     }
+
+    //callback d'un click sur le bouton
+    override fun onClick(v: View?) {
+        if(v === binding.btCancel) {
+            binding.et.setText("Clic sur annuler")
+        }
+    }
+
 }
